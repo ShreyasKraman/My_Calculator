356a066b2c9a73de5d4d604099c4df7d652d6ebd
Shreyas Kalyanaraman <shreyas.kalyanaraman@gmail.com>
2018-09-28 13:58:38 -0400

    Final release

--- src/component/App.js
+++ src/component/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Display from './Display';
-import ButtonPanel from './ButtonPanel';
-import calculate from '../calculate/calculate';
+//import ButtonPanel from './ButtonPanel';
 import './App.css';
 
 class App extends React.Component{
